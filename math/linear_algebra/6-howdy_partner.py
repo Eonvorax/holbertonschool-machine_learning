553111e22a066ed6219aefef78d46316839270a3
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2024-04-10 20:59:39 +0000

    Task 6: Concat arrays

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+This is the 6-howdy_partner module.
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenates two arrays (assuming arr1 and arr2 are lists of ints/floats)
+    """
+    return arr1 + arr2
