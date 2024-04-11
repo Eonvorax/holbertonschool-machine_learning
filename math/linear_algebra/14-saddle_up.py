2e8ccb880d5fb30320db7887aa56b58be055c72a
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2024-04-11 13:48:58 +0000

    Task 14: Saddle up

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+This is the 14-saddle_up module.
+"""
+from numpy import matmul
+
+
+def np_matmul(mat1, mat2):
+    """
+    Performs matrix multiplication.
+    """
+    return matmul(mat1, mat2)
