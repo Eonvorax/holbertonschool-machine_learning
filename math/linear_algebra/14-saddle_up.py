80d61659a280c584899eed030ec147310b7eaffc
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2024-04-11 13:52:54 +0000

    Checker hate

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -3,11 +3,11 @@
 """
 This is the 14-saddle_up module.
 """
-from numpy import matmul
+import numpy as np
 
 
 def np_matmul(mat1, mat2):
     """
     Performs matrix multiplication.
     """
-    return matmul(mat1, mat2)
+    return np.matmul(mat1, mat2)
