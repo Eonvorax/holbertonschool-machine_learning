496c7ea6fccdd7abd5a60a724e2784f8ff69218c
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2024-04-11 13:17:15 +0000

    Task 11: Transpose ndarray

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+This is the 11-the_western_exchange module.
+"""
+
+
+def np_transpose(matrix):
+    """
+    Transposes the given numpy.ndarray and return the new ndarray.
+    """
+    return matrix.T
