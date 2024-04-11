b1e2a29794566803c0e0a7cb44f7fcb11cab6110
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2024-04-11 13:06:51 +0000

    Task 10: Anubis & the feather

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+This the 10_ill_use_my_scale module
+"""
+
+
+def np_shape(matrix):
+    """
+    Calculates the shape of a numpy.ndarray and returns it as a
+    tuple of integers.
+    """
+    return matrix.shape
