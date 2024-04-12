41c23f624db753960215d5cb334f04a6476f9945
Bishop <mail4bishop@gmail.com>
2024-04-12 12:21:28 +0200

    Task 3: Two is better than one

---
+++ math/plotting/3-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+two = __import__('3-two').two
+
+two()
---
+++ math/plotting/3-two.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def two():
+
+    x = np.arange(0, 21000, 1000)
+    r = np.log(0.5)
+    t1 = 5730
+    t2 = 1600
+    y1 = np.exp((r / t1) * x)
+    y2 = np.exp((r / t2) * x)
+    plt.figure(figsize=(6.4, 4.8))
+
+    # your code here
+    plt.plot(x, y1, color='red', linestyle='--', label='C-14')
+    plt.plot(x, y2, color='green', linestyle='-', label='Ra-226')
+    plt.xlabel("Time (years)", fontsize=13)
+    plt.ylabel("Fraction Remaining", fontsize=13)
+    plt.title("Exponential Decay of Radioactive Elements", fontsize=16)
+    plt.legend(loc='upper right', fontsize=13)
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
+    plt.show()
