363cefd50713b0fc8b9e617a99a5e54cd019898c
Bishop <mail4bishop@gmail.com>
2024-04-12 12:36:16 +0200

    Task 4: Frequency

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    # your code here
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    plt.xlabel('Grades', fontsize=13)
+    plt.ylabel('Number of Students', fontsize=13)
+    plt.title('Project A', fontsize=16)
+    # Had to adjust axis limits
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
+
+    plt.show()
---
+++ math/plotting/4-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+frequency = __import__('4-frequency').frequency
+
+frequency()
