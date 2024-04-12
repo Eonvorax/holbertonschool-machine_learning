56c15ff330ced89d8b712efab0e070a1abda3778
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2024-04-12 15:35:27 +0000

    Removed styling for checker + forgot xticks

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -17,11 +17,14 @@ def frequency():
 
     # your code here
     plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
-    plt.xlabel('Grades', fontsize=13)
-    plt.ylabel('Number of Students', fontsize=13)
-    plt.title('Project A', fontsize=16)
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.title("Project A")
     # Had to adjust axis limits
     plt.xlim(0, 100)
     plt.ylim(0, 30)
+
+    # Forgot this
+    plt.xticks(range(0, 101, 10))
 
     plt.show()
