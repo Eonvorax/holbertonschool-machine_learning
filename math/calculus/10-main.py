b831a171063b071904dafd443ab3ef1cf497a99c
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2024-04-15 13:07:03 +0000

    Task 10: Derivative of polynomial

---
+++ math/calculus/10-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+poly_derivative = __import__('10-matisse').poly_derivative
+
+poly = [5, 3, 0, 1]
+print(poly_derivative(poly))
+print(poly_derivative([0]))
---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+This is the 10-matisse module.
+"""
+
+
+def poly_derivative(poly):
+    """
+    Calculates the derivative of a polynomial coefficient list.
+    """
+    if not isinstance(poly, list) \
+            or not all(isinstance(coeff, (int, float)) for coeff in poly):
+        return None
+
+    if len(poly) == 1:
+        return [0]
+
+    return [coef * i for i, coef in enumerate(poly) if i != 0]
