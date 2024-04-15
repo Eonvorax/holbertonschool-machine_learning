3f36ac83805625b92e7c7fff1987c8cc749d236c
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2024-04-15 11:49:19 +0000

    Removed 'for' comment for the checker

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -12,5 +12,5 @@ def summation_i_squared(n):
     if not isinstance(n, int) or n < 1:
         return None
 
-    # NOTE formula for the sum of the squares of the first n natural numbers
+    # NOTE formula: the sum of the squares of the first n natural numbers
     return n * (n + 1) * (2 * n + 1) // 6
