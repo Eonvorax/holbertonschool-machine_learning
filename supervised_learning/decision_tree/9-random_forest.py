fd21e22a77f41bb9852683f6dc3fbadeed9770dd
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2024-04-20 18:28:02 +0000

    Docs & pycodestyle

--- supervised_learning/decision_tree/9-random_forest.py
+++ supervised_learning/decision_tree/9-random_forest.py
@@ -4,12 +4,14 @@
 This is the 9-random_forest module, relying on module
 8-build_decision_tree.
 """
-
-Decision_Tree = __import__('8-build_decision_tree').Decision_Tree
 import numpy as np
+Decision_Tree = __import__('8-build_decision_tree').Decision_Tree
 
 
 class Random_Forest():
+    """
+    Random forest class, using Decision Trees.
+    """
     def __init__(self, n_trees=100, max_depth=10, min_pop=1, seed=0):
         self.numpy_predicts = []
         self.target = None
@@ -21,8 +23,16 @@ class Random_Forest():
 
     def predict(self, explanatory):
         """
-        Returns of array of the most frequent prediction for
-        each tree in numpy_preds, based on given explanatory.
+        Returns an array of the most frequent prediction for each tree in
+        numpy_preds, based on the given explanatory variables.
+
+        Args:
+            explanatory (array-like): The explanatory variables for
+            prediction.
+
+        Returns:
+            numpy.ndarray: An array of the most frequent predictions
+            for each tree.
         """
         # Generate predictions for each tree in the forest
         predictions = np.array(
@@ -30,13 +40,16 @@ class Random_Forest():
 
         # Calculate the mode (most frequent) prediction for each example
         mode_predictions = np.apply_along_axis(
-            lambda x: np.bincount(x).argmax(), # Mode
-            axis=0, # Axis is 0 since it's a 1D array
-            arr=predictions) # Applied to array of predictions
+            lambda x: np.bincount(x).argmax(),  # Mode
+            axis=0,  # Axis is 0 since it's a 1D array
+            arr=predictions)  # Applied to array of predictions
 
         return mode_predictions
 
     def fit(self, explanatory, target, n_trees=100, verbose=0):
+        """
+        Fits the random forest to the given training data.
+        """
         self.target = target
         self.explanatory = explanatory
         self.numpy_preds = []
@@ -59,8 +72,12 @@ class Random_Forest():
     - Mean number of nodes           : { np.array(nodes).mean()       }
     - Mean number of leaves          : { np.array(leaves).mean()      }
     - Mean accuracy on training data : { np.array(accuracies).mean()  }
-    - Accuracy of the forest on td   : {self.accuracy(self.explanatory,self.target)}""")
+    - Accuracy of the forest on td   : {self.accuracy(self.explanatory,
+                                                      self.target)}""")
 
     def accuracy(self, test_explanatory, test_target):
+        """
+        Calculates the accuracy of the random forest on the given test data.
+        """
         return np.sum(np.equal(self.predict(test_explanatory),
                                test_target)) / test_target.size
