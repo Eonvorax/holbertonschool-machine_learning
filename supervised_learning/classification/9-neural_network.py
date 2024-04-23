a5068c56790180ad730db1f1bd3a8e8f1f1a6bcb
Eonvorax <mail4bishop@gmail.com>
2024-04-23 15:50:12 +0200

    Task 9: Privatize Neural Network

---
+++ supervised_learning/classification/9-neural_network.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+"""
+This is the NeuralNetwork class module.
+"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+    Represents a neural network with one hidden layer performing binary
+    classification.
+
+    Attributes:
+        W1 (numpy.ndarray): The weights vector for the hidden layer.
+        b1 (numpy.ndarray): The bias for the hidden layer.
+        A1 (float): The activated output for the hidden layer.
+        W2 (numpy.ndarray): The weights vector for the output neuron.
+        b2 (float): The bias for the output neuron.
+        A2 (float): The activated output for the output neuron (prediction).
+    """
+
+    def __init__(self, nx, nodes):
+        """
+        Initializes a neural network with one hidden layer performing
+        binary classification.
+
+        Args:
+            nx (int): Number of input features.
+            nodes (int): Number of nodes in the hidden layer.
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """Getter for W1."""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Getter for b1."""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Getter for A1."""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Getter for W2."""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Getter for b2."""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Getter for A2."""
+        return self.__A2
