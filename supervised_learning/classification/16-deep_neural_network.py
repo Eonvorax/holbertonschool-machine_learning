329698877f4f2218cad5c9e62abbb9a12750feeb
Pierre <130477483+Eonvorax@users.noreply.github.com>
2024-04-25 09:12:39 +0000

    Trying with normal all(), numpy all seems to fail

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -37,7 +37,7 @@ class DeepNeuralNetwork:
 
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        if not np.all(map(lambda x: isinstance(x, int) and x > 0, layers)):
+        if not all(map(lambda x: isinstance(x, int) and x > 0, layers)):
             raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
