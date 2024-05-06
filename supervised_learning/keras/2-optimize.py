f7bc3e672e8f492e15175464bcbda5e886266132
Pierre <130477483+Eonvorax@users.noreply.github.com>
2024-05-06 13:19:10 +0000

    Fix: pycodestyle

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -20,8 +20,8 @@ def optimize_model(network, alpha, beta1, beta2):
     Returns: None
     """
     opt_adam = K.optimizers.Adam(learning_rate=alpha,
-                          beta_1=beta1,
-                          beta_2=beta2)
+                                 beta_1=beta1,
+                                 beta_2=beta2)
 
     network.compile(optimizer=opt_adam,
                     loss='categorical_crossentropy',
