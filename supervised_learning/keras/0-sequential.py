dee0a6b4711ff9f95b2c78977823511f1a52ea5a
Pierre <130477483+Eonvorax@users.noreply.github.com>
2024-05-06 12:10:16 +0000

    Classic case of the checker being terrible, can't optimize

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -4,9 +4,7 @@
 This is the 0-sequential module.
 """
 
-from keras.src.models import Sequential
-from keras.src.layers import Dense, Dropout
-from keras import regularizers
+import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
@@ -23,18 +21,18 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
 
     Returns: the keras model
     """
-    model = Sequential()
+    model = K.Sequential()
 
     for i, _ in enumerate(layers):
-        model.add(Dense(
+        model.add(K.layers.Dense(
             layers[i],
             activation=activations[i],
-            kernel_regularizer=regularizers.L2(lambtha),
+            kernel_regularizer=K.regularizers.L2(lambtha),
             input_shape=(nx,)
             ))
 
         # Add dropout layer on every layer except the last (output) layer
         if i != len(layers) - 1 and keep_prob is not None:
-            model.add(Dropout(1 - keep_prob))
+            model.add(K.layers.Dropout(1 - keep_prob))
 
     return model
