783faaef9ca8269eb75edae1bf4b61de882c6b71
Bishop <mail4bishop@gmail.com>
2024-05-28 15:03:31 +0200

    Stricter flooring in calculation, type checking

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -26,17 +26,17 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     sh, sw = stride
 
     if padding == "same":
-        ph = ((h - 1) * sh + kh - h) // 2
-        pw = ((w - 1) * sw + kw - w) // 2
+        ph = int((((h - 1) * sh + kh - h) / 2) + 1)
+        pw = int((((w - 1) * sw + kw - w) / 2) + 1)
     elif padding == "valid":
         ph = 0
         pw = 0
-    else:
+    elif isinstance(padding, tuple):
         ph, pw = padding
 
-    # Calculate output dimensions
-    output_h = (h + 2 * ph - kh) // sh + 1
-    output_w = (w + 2 * pw - kw) // sw + 1
+    # Default output dimensions
+    output_h = int((h + 2 * ph - kh) / sh) + 1
+    output_w = int((w + 2 * pw - kw) / sh) + 1
 
     # NOTE padding indexes: (before, after), shortcut is (padding,)
     padded_imgs = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), mode='constant')
@@ -44,8 +44,8 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     # Initialize convolution output array
     convolved = np.zeros((m, output_h, output_w))
 
-    for i in range(0, output_h):
-        for j in range(0, output_w):
+    for i in range(output_h):
+        for j in range(output_w):
             # Extract region from padded images, scaling indexes by stride
             region = padded_imgs[:, i*sh:i*sh+kh, j*sw:j*sw+kw]
 
