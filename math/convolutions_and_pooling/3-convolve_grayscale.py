8b1304213777949a315f252b525d6db81d5b9e0f
Bishop <mail4bishop@gmail.com>
2024-05-28 15:17:22 +0200

    Reverted to previous method, type checking first

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -25,18 +25,18 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     kh, kw = kernel.shape
     sh, sw = stride
 
-    if padding == "same":
-        ph = int((((h - 1) * sh + kh - h) / 2) + 1)
-        pw = int((((w - 1) * sw + kw - w) / 2) + 1)
-    elif padding == "valid":
+    if isinstance(padding, tuple):
+        ph, pw = padding
+    elif padding == 'same':
+        ph = ((h - 1) * sh + kh - h) // 2 + 1
+        pw = ((w - 1) * sw + kw - w) // 2 + 1
+    elif padding == 'valid':
         ph = 0
         pw = 0
-    elif isinstance(padding, tuple):
-        ph, pw = padding
 
-    # Default output dimensions
-    output_h = int((h + 2 * ph - kh) / sh) + 1
-    output_w = int((w + 2 * pw - kw) / sh) + 1
+    # Calculate output dimensions
+    output_h = (h + 2 * ph - kh) // sh + 1
+    output_w = (w + 2 * pw - kw) // sw + 1
 
     # NOTE padding indexes: (before, after), shortcut is (padding,)
     padded_imgs = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), mode='constant')
