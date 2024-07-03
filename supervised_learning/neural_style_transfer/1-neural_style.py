4a12065c2f9c5838730c0b23039d6d1d1dd50214
Eonvorax <mail4bishop@gmail.com>
2024-07-03 13:17:01 +0200

    Gave up, took terrible but working solution

--- supervised_learning/neural_style_transfer/1-neural_style.py
+++ supervised_learning/neural_style_transfer/1-neural_style.py
@@ -127,23 +127,18 @@ class NST:
         """
         Load the VGG19 model with AveragePooling2D instead of MaxPooling2D.
         """
-        # Load VGG19 model from Keras API
-        vgg = tf.keras.applications.VGG19(
-            include_top=False, weights='imagenet')
+        vgg19 = tf.keras.applications.VGG19(include_top=False)
+        vgg19.trainable = False
+        vgg19.save("vgg_base_model.h5")
+        model = tf.keras.models.load_model(
+            "vgg_base_model.h5",
+            custom_objects={
+                "MaxPooling2D": tf.keras.layers.AveragePooling2D(
+                    pool_size=(2, 2)
+                )
+            })
 
-        # Replace MaxPooling2D layers with AveragePooling2D layers
-        for layer in vgg.layers:
-            if isinstance(layer, tf.keras.layers.MaxPooling2D):
-                layer.__class__ = tf.keras.layers.AveragePooling2D
-
-        # get outputs of the style and content layers from modified VGG19
-        style_outputs = [vgg.get_layer(
-            name).output for name in self.style_layers]
-        content_output = vgg.get_layer(self.content_layer).output
-
-        # Create the model, make it non-trainable and return it
-        model = tf.keras.Model(name="model", inputs=vgg.input,
-                               outputs=style_outputs + [content_output])
-        model.trainable = False
-
-        return model
+        outputs = ([model.get_layer(layer).output
+                   for layer in self.style_layers]
+                   + [model.get_layer(self.content_layer).output])
+        return tf.keras.models.Model(model.input, outputs)
