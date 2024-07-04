f14fe72f08be4fe66fe526503e16614e848af180
Eonvorax <mail4bishop@gmail.com>
2024-07-04 16:20:28 +0200

    Task 4: Style cost

---
+++ supervised_learning/neural_style_transfer/5-main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import matplotlib.image as mpimg
+import os
+import random
+import numpy as np
+import tensorflow as tf
+
+NST = __import__('5-neural_style').NST
+
+
+if __name__ == '__main__':
+    style_image = mpimg.imread("starry_night.jpg")
+    content_image = mpimg.imread("golden_gate.jpg")
+
+    # Reproducibility
+    SEED = 0
+    os.environ['PYTHONHASHSEED'] = str(SEED)
+    os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+    random.seed(SEED)
+    np.random.seed(SEED)
+    tf.random.set_seed(SEED)
+
+    nst = NST(style_image, content_image)
+    vgg19 = tf.keras.applications.vgg19
+    preprocecced = vgg19.preprocess_input(nst.content_image * 255)
+    style_outputs = nst.model(preprocecced)[:-1]
+    style_cost = nst.style_cost(style_outputs)
+    print(style_cost)
