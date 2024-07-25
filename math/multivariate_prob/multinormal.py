3a243683e9a91cec71011ae89d52ae9937c7a61b
Bishop <mail4bishop@gmail.com>
2024-07-25 12:05:52 +0200

    Task 3: PDF

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -33,3 +33,35 @@ class MultiNormal:
 
         # Using compact formula for covariance
         self.cov = np.dot(data_centered, data_centered.T) / (n - 1)
+
+    def pdf(self, x):
+        """
+        Calculates the PDF at a data point.
+
+        Parameters:
+        - `x` (`numpy.ndarray`): The data point of shape `(d, 1)`
+
+        Returns:
+        - `float`: The value of the PDF at the data point `x`
+        """
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+
+        d = self.mean.shape[0]
+        if x.shape != (d, 1):
+            raise ValueError(f"x must have the shape ({d}, 1)")
+
+        # Determinant and inverse of the covariance matrix
+        cov_det = np.linalg.det(self.cov)
+        cov_inv = np.linalg.inv(self.cov)
+
+        # Calc. the normalization constant
+        norm_const = 1.0 / (np.sqrt((2 * np.pi) ** d * cov_det))
+
+        # Calculate the exponent
+        diff = x - self.mean
+        exponent = -0.5 * (diff.T @ cov_inv @ diff)
+
+        # Finally, the PDF value (not the scalar)
+        pdf_value = norm_const * np.exp(exponent)
+        return pdf_value[0, 0]
