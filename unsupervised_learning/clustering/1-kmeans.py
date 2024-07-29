73ca62efb75d520afa8783858930324ebbc01f5d
Bishop <mail4bishop@gmail.com>
2024-07-29 16:27:57 +0200

    Not my code, trying to see if the checker is broken

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -54,40 +54,28 @@ def kmeans(X, k, iterations=1000):
            - clss is a numpy.ndarray of shape (n,) containing the index of the
              cluster in C that each data point belongs to.
     """
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
+    if not isinstance(iterations, int) or iterations <= 0:
         return None, None
     if not isinstance(k, int) or k <= 0:
         return None, None
-    if not isinstance(iterations, int) or iterations <= 0:
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
         return None, None
 
-    centroids = initialize(X, k)
-    if centroids is None:
-        return None, None
+    C = initialize(X, k)
 
     for _ in range(iterations):
-        # Computing new centroids
-        new_ctds = np.copy(centroids)
+        prev_C = np.copy(C)
 
-        dists = np.sqrt(np.sum((X - centroids[:, np.newaxis]) ** 2, axis=2))
+        dists = np.sqrt(np.sum((X - C[:, np.newaxis]) ** 2, axis=2))
         clss = np.argmin(dists, axis=0)
 
         for i in range(k):
-            # Mask: points present in cluster
-            cluster_mask = X[clss == i]
-            if len(cluster_mask) == 0:
-                new_ctds[i] = initialize(X, 1)
+            if np.sum(clss == i) > 0:
+                C[i] = np.mean(X[clss == i], axis=0)
             else:
-                new_ctds[i] = np.mean(cluster_mask, axis=0)
-
-        dists = np.sqrt(np.sum((X - centroids[:, np.newaxis]) ** 2, axis=2))
+                C[i] = initialize(X, 1)
+        dists = np.sqrt(np.sum((X - C[:, np.newaxis]) ** 2, axis=2))
         clss = np.argmin(dists, axis=0)
-        # Convergence check (points haven't changed clusters)
-        if np.allclose(new_ctds, centroids):
-            # Found the final centroids, returning them
-            return centroids, clss
-
-        # Didn't achieve convergence, switching to new centroids
-        centroids = new_ctds
-
-    return centroids, clss
+        if np.all(C == prev_C):
+            break
+    return C, clss
