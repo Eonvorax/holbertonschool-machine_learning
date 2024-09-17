88de570d395fd04366119c018b659013f9df3dc2
Bishop <mail4bishop@gmail.com>
2024-09-17 14:50:46 +0200

    Task 3: Extract & convert Word2Vec model to a keras layer

---
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Extract & convert Word2Vec model to a keras layer
+"""
+from keras.layers import Embedding
+
+
+def gensim_to_keras(model):
+    """
+    Converts a gensim Word2Vec model to a keras Embedding layer
+
+    :Parameters:
+    - `model`: a trained gensim Word2Vec model
+
+    Returns:
+        The trainable keras Embedding
+    """
+    keyed_vectors = model.wv  # structure holding the result of training
+    weights = keyed_vectors.vectors  # vectors themselves, a 2D numpy array
+
+    layer = Embedding(
+        input_dim=weights.shape[0],
+        output_dim=weights.shape[1],
+        weights=[weights],
+        trainable=True
+    )
+    return layer
