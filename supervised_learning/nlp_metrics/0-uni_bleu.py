7778789f8b8de23f8adc7875ee293ca9ae66d25c
Eonvorax <mail4bishop@gmail.com>
2024-09-18 19:03:39 +0200

    Task 0: Unigram BLEU score

---
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Unigram BLEU score
+"""
+import numpy as np
+
+
+def uni_bleu(references, sentence):
+    """
+    Calculates the unigram BLEU score for a sentence:
+
+    :Parameters:
+    - `references` is a list of reference translations
+        - each reference translation is a list of the words in the translation
+    - `sentence` is a list containing the model proposed sentence
+
+    Returns: the unigram BLEU score
+    """
+
+    sentence_len = len(sentence)
+
+    # Calculate maximum matches for each word in the sentence
+    matches = 0
+    for word in sentence:
+        max_count = max(ref.count(word) for ref in references)
+        matches += min(sentence.count(word), max_count)
+
+    # Calculate precision (correct matches / total words in sentence)
+    precision = matches / sentence_len
+
+    # Find the reference length closest to the sentence length
+    closest_ref_len = min(
+        (abs(len(ref) - sentence_len), len(ref)) for ref in references)[1]
+
+    # Calculate brevity penalty
+    if sentence_len > closest_ref_len:
+        brevity_penalty = 1
+    else:
+        brevity_penalty = np.exp(1 - closest_ref_len / sentence_len)
+
+    # Final BLEU score, accounting for BP if necessary
+    return brevity_penalty * precision
