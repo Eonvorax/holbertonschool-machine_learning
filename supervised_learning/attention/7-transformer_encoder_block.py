67dfec68ff3a78d034eee865c162bb885c80bf78
Bishop <mail4bishop@gmail.com>
2024-09-26 10:42:14 +0200

    The usual checker stupidity

--- supervised_learning/attention/7-transformer_encoder_block.py
+++ supervised_learning/attention/7-transformer_encoder_block.py
@@ -3,8 +3,7 @@
 Transformer Encoder Block
 """
 import tensorflow as tf
-MultiHeadAttention = __import__(
-    '6-multihead_attention').MultiHeadAttention
+MultiHeadAttention = __import__('6-multihead_attention').MultiHeadAttention
 
 
 class EncoderBlock(tf.keras.layers.Layer):
