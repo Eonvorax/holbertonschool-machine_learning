c15adfe0022fff72faba1958df7e1c85502588dd
Bishop <mail4bishop@gmail.com>
2024-09-30 15:49:58 +0200

    Task 1: Encode Tokens (no review button for now, RIP)

---
+++ supervised_learning/transformer_apps/1-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+Dataset = __import__('1-dataset').Dataset
+
+data = Dataset()
+for pt, en in data.data_train.take(1):
+    print(data.encode(pt, en))
+for pt, en in data.data_valid.take(1):
+    print(data.encode(pt, en))
