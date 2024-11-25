caf4c975e13132d7bddf275c7afdc26ab7fe1eed
Eonvorax <mail4bishop@gmail.com>
2024-11-25 10:21:39 +0100

    Task 4: Select score, name over 10, by descending order

---
+++ pipeline/databases/4-best_score.sql
@@ -0,0 +1,2 @@
+-- Lists all records (score and name) with a score >= 10 in the table second_table, ordered by score in descending order.
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
