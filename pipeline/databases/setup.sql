c7ec36da640adf98f6daa357a9ebe2ac2b68f919
Eonvorax <mail4bishop@gmail.com>
2024-11-25 10:30:56 +0100

    Task 5: Select average from scores in table

--- pipeline/databases/setup.sql
+++ pipeline/databases/setup.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS second_table (
     score INT
 );
 INSERT INTO second_table (id, name, score) VALUES (1, "Bob", 14);
-INSERT INTO second_table (id, name, score) VALUES (2, "Roy", 3);
+INSERT INTO second_table (id, name, score) VALUES (2, "Roy", 5);
 INSERT INTO second_table (id, name, score) VALUES (3, "John", 10);
 INSERT INTO second_table (id, name, score) VALUES (4, "Bryan", 8);
