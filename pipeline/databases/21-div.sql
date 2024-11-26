514903f9d39cf43de06decfc7ca420ffa21fef75
Eonvorax <mail4bishop@gmail.com>
2024-11-26 15:47:12 +0100

    Task 21: Create a function SafeDiv for division

---
+++ pipeline/databases/21-div.sql
@@ -0,0 +1,16 @@
+-- creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
+DELIMITER //
+
+DROP FUNCTION IF EXISTS SafeDiv //
+
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END //
+
+DELIMITER ;
