ac24deef15abeaa092fe7141014ea2b983564f5d
Eonvorax <mail4bishop@gmail.com>
2024-11-26 09:41:27 +0100

    Task 14: Create table with an enum country colum that defaults to US

---
+++ pipeline/databases/14-country_users.sql
@@ -0,0 +1,7 @@
+-- Create a similar table "users", with a "country" non-null enum column that defaults to 'US'
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
